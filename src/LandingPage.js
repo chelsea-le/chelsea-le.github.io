ed093b7a2f68fb4eff70d62382934fd88f488dba
Chelsea Le <chele510@uw.edu>
2016-11-21 15:23:09 -0800

    fixed social icons

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -20,9 +20,9 @@ var LandingPage = React.createClass({
                 </h1>
                 <h2>University of Washington - Informatics Student - Woman in Tech - Technophile</h2>
                 <div className="icons">
-                  <a href="https://www.linkedin.com/in/chelseale" target="_blank" id="linkedin"> <i className="fa fa-linkedin-square"></i></a>
-                  <a href="https://www.instagram.com/chelsea.le510" target="_blank" id="instagram"> <i className="fa fa-instagram"></i></a>
-                  <a href="https://www.facebook.com/chelsea.le.15" target="_blank" id="facebook"> <i className="fa fa-facebook-square"></i></a>
+                  <a href="https://www.linkedin.com/in/chelseale" target="_blank" id="linkedin"><i className="fa fa-linkedin-square"></i></a>
+                  <a href="https://www.instagram.com/chelsea.le510" target="_blank" id="instagram"><i className="fa fa-instagram"></i></a>
+                  <a href="https://www.facebook.com/chelsea.le.15" target="_blank" id="facebook"><i className="fa fa-facebook-square"></i></a>
                 </div>
             </div>
         )
